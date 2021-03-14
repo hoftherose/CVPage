8605fec6dd50a6066ef19d9ae966511d0ab0656a
Héctor de la Rosa <hoftherose@gmail.com>
2021-03-14 10:50:33 -0600

    Variables and redirects

--- Basics/app.py
+++ Basics/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -11,6 +11,17 @@ def say_bye():
     return "Good Bye"
 
 app.add_url_rule('/Bye', view_func=say_bye)
+
+@app.route('/Wassap/<string:name>')
+def wassap(name):
+    return f"Wassap {name}"
+
+@app.route('/<int:leaving>')
+def in_or_out(leaving):
+    if leaving:
+        return redirect(url_for('home'))
+    else:
+        return redirect(url_for('say_bye'))
 
 if __name__ == '__main__':
     app.run(debug=True)
