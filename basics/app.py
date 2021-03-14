eebf3838da13d7721ca14a53ca882bcbc9e72a6c
Héctor de la Rosa <hoftherose@gmail.com>
2021-03-14 17:53:20 -0600

    Post methods and forms

--- basics/app.py
+++ basics/app.py
@@ -1,14 +1,29 @@
-from flask import Flask, redirect, url_for, render_template
+import json
+from flask import Flask, render_template, request#, redirect, url_for
+from flask_restful import Api
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
-@app.route('/welcome/<int:usr>')
-def welcome(usr):
-    return render_template("welcome.html", name=usr)
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        return request.form["form"]
+    return render_template("form.html")
+
+@app.route('/get', methods=['GET', 'POST'])
+def employee_data():
+    if request.method == 'POST':
+        file = open('basics/data/data.json', 'w')
+        data = json.load(file)
+        data[len(data)] = request.form["data"]
+    file = open('basics/data/data.json', 'r')
+    data = json.load(file)
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
