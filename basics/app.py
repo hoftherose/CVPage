a9041f8bf74a3284d2cf036e9ef6cda7a0afd0fa
Héctor de la Rosa <hoftherose@gmail.com>
2021-03-17 21:29:19 -0600

    Database setup

--- basics/app.py
+++ basics/app.py
@@ -1,13 +1,28 @@
-import json
+# import os
 from flask import Flask, render_template, request#, redirect, url_for
-from flask_restful import Api
-
-from forms.forms import modify_data
+from flask_restful import Api, Resource, reqparse, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+db_uri = 'sqlite:///data/database.db'
 
-fn = 'basics/data/data.json'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+db = SQLAlchemy(app)
+db.create_all()
+
+class EmployeeModel(db.Model):
+    usr_id      = db.Column(db.Integer(), primary_key=True)
+    job_title   = db.Column(db.String(100), nullable=False)
+    fname       = db.Column(db.String(100), nullable=False)
+    lname       = db.Column(db.String(100), nullable=False)
+    empl_code   = db.Column(db.String(2), nullable=False)
+    region      = db.Column(db.String(2), nullable=False)
+    phone_num   = db.Column(db.String(7), nullable=False)
+    email       = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'{self.job_title} {self.usr_id}-{self.empl_code}: {self.lname} {self.fname}'
 
 @app.route('/')
 def home():
