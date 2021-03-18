26948f8e62248e9660af13e5b9265513a02c2bc6
Héctor de la Rosa <hoftherose@gmail.com>
2021-03-18 11:14:22 -0600

    Cleanup

--- basics/forms/forms.py
+++
@@ -1,9 +0,0 @@
-data_entries = ['userId','jobTitle','firstName','lastName','employeeCode','region','phoneNumber','emailAddress']
-
-def modify_data(data, request):
-    for entry in data_entries:
-        if request.form[entry] == None: return False
-    data['Employees'].append({})
-    for entry in data_entries:
-        data['Employees'][-1][entry] = request.form[entry]
-    return True
