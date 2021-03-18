cf8d1d2296f0a4509db487fc318b2c21977ea9d9
Héctor de la Rosa <hoftherose@gmail.com>
2021-03-17 21:30:06 -0600

    API dataparser

--- basics/app.py
+++ basics/app.py
@@ -11,6 +11,27 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db = SQLAlchemy(app)
 db.create_all()
 
+data_parser = reqparse.RequestParser()
+data_parser.add_argument('usr_id', type=int, help='No id', required=True)
+data_parser.add_argument('job_title', type=str, help='No title', required=True)
+data_parser.add_argument('fname', type=str, help='First name missing', required=True)
+data_parser.add_argument('lname', type=str, help='Last name missing', required=True)
+data_parser.add_argument('empl_code', type=str, help='employee code missing', required=True)
+data_parser.add_argument('region', type=str, help='region missing', required=True)
+data_parser.add_argument('phone_num', type=str, help='phone number missing', required=True)
+data_parser.add_argument('email', type=str, help='email missing', required=True)
+
+mfields = {
+    'usr_id':fields.Integer,
+    'job_title':fields.String,
+    'fname':fields.String,
+    'lname':fields.String,
+    'empl_code':fields.String,
+    'region':fields.String,
+    'phone_num':fields.String,
+    'email':fields.String
+}
+
 class EmployeeModel(db.Model):
     usr_id      = db.Column(db.Integer(), primary_key=True)
     job_title   = db.Column(db.String(100), nullable=False)
@@ -20,6 +41,7 @@ class EmployeeModel(db.Model):
     region      = db.Column(db.String(2), nullable=False)
     phone_num   = db.Column(db.String(7), nullable=False)
     email       = db.Column(db.String(100), nullable=False)
+    # created_at  = db.Column(db.DateTime())
 
     def __repr__(self):
         return f'{self.job_title} {self.usr_id}-{self.empl_code}: {self.lname} {self.fname}'
