49a9e9ab1184329638d673b56df2fa0812d43e29
Héctor de la Rosa <hoftherose@gmail.com>
2021-03-28 12:17:43 -0600

    Test image static file

--- intermediate/ImageProcessing/views.py
+++ intermediate/ImageProcessing/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for
 ImageProcessor = Blueprint("image_views", __name__)
 
 @ImageProcessor.route("/image/")
@@ -7,4 +7,4 @@ def ImageHome():
 
 @ImageProcessor.route("/image/upload")
 def UploadImage():
-    return "upload here"
+    return render_template("images/upload.html")
