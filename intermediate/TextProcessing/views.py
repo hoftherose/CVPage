f1c3f61f4094322f9f77c0177b4b792170415470
Héctor de la Rosa <hoftherose@gmail.com>
2021-03-28 09:32:49 -0600

    Quick Processing

--- intermediate/TextProcessing/views.py
+++ intermediate/TextProcessing/views.py
@@ -4,3 +4,11 @@ TextProcessor = Blueprint("views", __name__)
 @TextProcessor.route("/text/")
 def TextHome():
     return "hi"
+
+@TextProcessor.route("/text/caps/<string:text>", methods=["GET", "POST"])
+def Capitalize(text):
+    return text.upper()
+
+@TextProcessor.route("/text/lower/<string:text>", methods=["GET", "POST"])
+def LowerCase(text):
+    return text.lower()
