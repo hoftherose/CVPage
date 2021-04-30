cc22f256fa0c1eb76bec9f6c222d2245816459e2
Hector de la Rosa <hector.delarosa@wizeline.com>
2021-04-29 19:17:03 -0500

    HomePage views

---
+++ HomePages/views.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+import json
+
+HomePage = Blueprint("home_pages", __name__)
+
+@HomePage.route("/")
+def Home():
+    return "HomePage :D"
+
+@HomePage.route("/projects")
+def Projects():
+    return render_template("main/projects.html")
--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from HomePages import HomePage
 from TextProcessing import TextProcessor
 from ImageProcessing import ImageProcessor
 
 def create_app():
     app = Flask(__name__)
+    
+    app.register_blueprint(HomePage)
     app.register_blueprint(TextProcessor)
     app.register_blueprint(ImageProcessor)
 
